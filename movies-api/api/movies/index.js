acaffcaadc1da24f11a4969cf7673c44e6aca1a8
salome <salome123987@gmail.com>
2022-05-03 14:46:36 +0100

    Topic 11 - Finished!

--- movies-api/api/movies/index.js
+++ movies-api/api/movies/index.js
@@ -3,7 +3,7 @@ import { movies, movieReviews, movieDetails } from './moviesData';
 import uniqid from 'uniqid';
 import movieModel from './movieModel';
 import asyncHandler from 'express-async-handler';
-
+import { getUpcomingMovies } from '../tmdb-api';
 
 const router = express.Router(); 
 router.get('/', asyncHandler(async (req, res) => {
@@ -54,4 +54,8 @@ router.get('/:id/reviews', (req, res) => {
     }
 });
 
+router.get('/tmdb/upcoming', asyncHandler( async(req, res) => {
+    const upcomingMovies = await getUpcomingMovies();
+    res.status(200).json(upcomingMovies);
+  }));
 export default router;
