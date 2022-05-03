38e44005cad3a0cd0b97c0869ca2f79f3b01a4e2
salome <salome123987@gmail.com>
2022-05-03 13:56:20 +0100

    Topic 10 - Update Movies Data/Route

--- movies-api/seedData/index.js
+++ movies-api/seedData/index.js
@@ -1,6 +1,8 @@
 import userModel from '../api/users/userModel';
 import users from './users';
 import dotenv from 'dotenv';
+import movieModel from '../api/movies/movieModel';
+import movies from './movies.js';
 
 dotenv.config();
 
@@ -18,4 +20,18 @@ async function loadUsers() {
 
 if (process.env.SEED_DB) {
   loadUsers();
+  loadMovies();//ADD THIS LINE
+}
+
+// deletes all movies documents in collection and inserts test data
+export async function loadMovies() {
+  console.log('load seed data');
+  console.log(movies.length);
+  try {
+    await movieModel.deleteMany();
+    await movieModel.collection.insertMany(movies);
+    console.info(`${movies.length} Movies were successfully stored.`);
+  } catch (err) {
+    console.error(`failed to Load movie Data: ${err}`);
+  }
 }
